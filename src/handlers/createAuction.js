55a9ec48f273fbf7b9f683083a9c88580a3116c7
Jintan09 <sfritsch09@gmail.com>
2021-07-28 22:29:47 +0200

    added Schema

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -2,18 +2,23 @@ import AWS from 'aws-sdk';
 import createError from 'http-errors';
 import { v4 as uuid } from 'uuid';
 import commonMiddleware from '../lib/commonMiddleware';
+import validator from '@middy/validator';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
 	const { title } = event.body;
 	const now = new Date();
+	const endDate = new Date();
+	endDate.setHours(now.getHours() + 1);
 
 	const auction = {
 		id: uuid(),
 		title,
 		status: 'OPEN',
 		createdAt: now.toISOString(),
+		endingAt: endDate.toISOString(),
 		highestBid: {
 			amount: 0,
 		},
@@ -27,7 +32,7 @@ async function createAuction(event, context) {
 			})
 			.promise();
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 		throw new createError.InternalServerError(error);
 	}
 
@@ -37,4 +42,6 @@ async function createAuction(event, context) {
 	};
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction).use(
+	validator({ inputSchema: createAuctionSchema })
+);
--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -2,6 +2,8 @@ import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
+import validator from '@middy/validator';
+import placeBidSchema from '../lib/schemas/placeBidSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -10,6 +12,10 @@ async function placeBid(event, context) {
 	const { amount } = event.body;
 
 	const auction = await getAuctionById(id);
+
+	if (auction.status !== 'OPEN') {
+		throw new createError.Forbidden('You cannot bid on closed auctions!');
+	}
 
 	if (amount <= auction.highestBid.amount) {
 		throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
@@ -39,4 +45,4 @@ async function placeBid(event, context) {
 	};
 }
 
-export const handler = commonMiddleware(placeBid);
+export const handler = commonMiddleware(placeBid).use(validator({ inputSchema: placeBidSchema }));
